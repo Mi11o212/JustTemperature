e6c7754ebe7c3e60e8771ad1847417af3b542def
Miller Fourie <miller.fourie@gmail.com>
2025-02-20 16:09:06 -0700

    Restarting

--- README.md
+++
@@ -1,5 +0,0 @@
-<p align="center"><img src="https://i.imgur.com/r0ZlRwr.png"></p>
-
-**Tough As Nails** is a **Minecraft mod** that adds various features to increase the game's difficulty, including body temperature and thirst.
-
-© 2024 Glitchfiend. All rights reserved.
--- common/src/main/java/toughasnails/init/ModItems.java
+++
@@ -1,8 +0,0 @@
-        TANItems.LEAF_HELMET = registerItem(func, "leaf_helmet", p -> new LeafArmorItem(ModArmorMaterials.LEAF, ArmorType.HELMET, p), new Item.Properties().stacksTo(1));
-        TANItems.LEAF_CHESTPLATE = registerItem(func, "leaf_chestplate", p -> new LeafArmorItem(ModArmorMaterials.LEAF, ArmorType.CHESTPLATE, p), new Item.Properties().stacksTo(1));
-        TANItems.LEAF_LEGGINGS = registerItem(func, "leaf_leggings", p -> new LeafArmorItem(ModArmorMaterials.LEAF, ArmorType.LEGGINGS, p), new Item.Properties().stacksTo(1));
-        TANItems.LEAF_BOOTS = registerItem(func, "leaf_boots", p -> new LeafArmorItem(ModArmorMaterials.LEAF, ArmorType.BOOTS, p), new Item.Properties().stacksTo(1));
-        TANItems.WOOL_HELMET = registerItem(func, "wool_helmet", p -> new WoolArmorItem(ModArmorMaterials.WOOL, ArmorType.HELMET, p), new Item.Properties().stacksTo(1));
-        TANItems.WOOL_CHESTPLATE = registerItem(func, "wool_chestplate", p -> new WoolArmorItem(ModArmorMaterials.WOOL, ArmorType.CHESTPLATE, p), new Item.Properties().stacksTo(1));
-        TANItems.WOOL_LEGGINGS = registerItem(func, "wool_leggings", p -> new WoolArmorItem(ModArmorMaterials.WOOL, ArmorType.LEGGINGS, p), new Item.Properties().stacksTo(1));
-        TANItems.WOOL_BOOTS = registerItem(func, "wool_boots", p -> new WoolArmorItem(ModArmorMaterials.WOOL, ArmorType.BOOTS, p), new Item.Properties().stacksTo(1));
