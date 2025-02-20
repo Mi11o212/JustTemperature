059fad6cf91fd21fe4b4458c363ff13d2e5f5ca2
Miller Fourie <miller.fourie@gmail.com>
2025-02-20 16:18:28 -0700

    50 50 it works nowa

--- common/src/main/java/toughasnails/api/village/TANVillagerProfessions.java
+++ common/src/main/java/toughasnails/api/village/TANVillagerProfessions.java
@@ -4,9 +4,6 @@
  ******************************************************************************/
 package toughasnails.api.village;
 
-import net.minecraft.world.entity.npc.VillagerProfession;
 
-public class TANVillagerProfessions
 {
-    public static VillagerProfession CLIMATOLOGIST;
 }
